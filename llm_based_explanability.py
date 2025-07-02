e8b93d9501c08f292160e5fc7e502bef97897a19
Elaheh Hosseni <elahe.s.hs98@gmail.com>
2025-07-03 00:54:38 +0330

    Delete separate config file.

--- llm_based_explanability.py
+++ llm_based_explanability.py
@@ -1,26 +1,29 @@
-from openrouter_config import client
+from openai import OpenAI
 
-def query_openAI(prompt, model):
-    try:
-        completion = client.chat.completions.create(
-            model=model,
-            messages=[
-                {
-                    "role": "user",
-                    "content": [
-                                {
-                                "type": "text",
-                                "text": prompt
-                                }
-                    ]
-                }
-            ]
-        )
-        return completion.choices[0].message.content
-    except Exception as e:
-        print(f"Error: {e}")
-        return "Failed to generate a response."
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="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",
+)
 
-
-
-
+completion = client.chat.completions.create(
+  extra_body={},
+  model="mistralai/mistral-small-3.2-24b-instruct:free",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What is in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+          }
+        }
+      ]
+    }
+  ]
+)
+print(completion.choices[0].message.content)
